f3b59816d8c8c225cd852dc9e8ef12057bdce1f6
zagob <matheusbestana@gmail.com>
2022-12-14 18:38:25 -0300

    ajust disable button point + time status

--- src/components/Dashboard/AsideAddPoint.tsx
+++ src/components/Dashboard/AsideAddPoint.tsx
@@ -15,6 +15,81 @@ export interface DataFormProps {
   exitOne: string;
   entryTwo: string;
   exitTwo: string;
+  holiday: boolean;
+}
+
+interface ClockTimeStatusProps {
+  bonusTotalMinutesStatus: number;
+  hours: string;
+  minutes: string;
+}
+
+function ClockTimeStatus({
+  bonusTotalMinutesStatus,
+  hours,
+  minutes,
+}: ClockTimeStatusProps) {
+  return (
+    <div
+      className={`border-2 border-green-400 ${
+        bonusTotalMinutesStatus === 1
+          ? "border-green-700 text-green-400"
+          : bonusTotalMinutesStatus === -1
+          ? "border-red-700 text-red-400"
+          : "border-slate-700 text-slate-400"
+      } relative w-16 h-8 flex z-10 justify-center gap-1 items-center bg-zinc-900`}
+    >
+      <div
+        className={`absolute w-2 h-1 -top-1 left-2 ${
+          bonusTotalMinutesStatus === 1
+            ? "bg-green-400"
+            : bonusTotalMinutesStatus === -1
+            ? " bg-red-400"
+            : " bg-slate-400"
+        }`}
+      />
+      <span className="text-center">{hours}</span>
+      <div className="flex flex-col gap-1">
+        <div
+          className={`w-1 h-1 rounded-full animate-pulse ${
+            bonusTotalMinutesStatus === 1
+              ? "bg-green-400"
+              : bonusTotalMinutesStatus === -1
+              ? " bg-red-400"
+              : " bg-slate-400"
+          }`}
+        />
+        <div
+          className={`w-1 h-1 rounded-full animate-pulse ${
+            bonusTotalMinutesStatus === 1
+              ? "bg-green-400"
+              : bonusTotalMinutesStatus === -1
+              ? " bg-red-400"
+              : " bg-slate-400"
+          }`}
+        />
+      </div>
+      <span className="text-center">{minutes}</span>
+      <div
+        className={`absolute w-[10px] h-[1px] -rotate-45 left-0 -bottom-[6px] z-0 ${
+          bonusTotalMinutesStatus === 1
+            ? "bg-green-400"
+            : bonusTotalMinutesStatus === -1
+            ? " bg-red-400"
+            : " bg-slate-400"
+        }`}
+      />
+      <div
+        className={`absolute w-[10px] h-[1px] rotate-45 right-0 -bottom-[6px] z-0 ${
+          bonusTotalMinutesStatus === 1
+            ? "bg-green-400"
+            : bonusTotalMinutesStatus === -1
+            ? " bg-red-400"
+            : " bg-slate-400"
+        }`}
+      />
+    </div>
+  );
 }
 
 export function AsideAddPoint() {
@@ -27,9 +102,32 @@ export function AsideAddPoint() {
     bonusTotalMinutes,
     onAddPointTime,
   } = useTime();
-  const { handleSubmit, register, reset } = useForm<DataFormProps>();
+  const { handleSubmit, register, setValue, reset, watch } =
+    useForm<DataFormProps>({
+      defaultValues: {
+        entryOne: "",
+        entryTwo: "",
+        exitOne: "",
+        exitTwo: "",
+        holiday: false,
+      },
+    });
 
   async function handleSubmitData(data: DataFormProps) {
+    const { holiday } = data;
+    if (holiday) {
+      const dataTimeFormat = {
+        entryOne: "00:00",
+        exitOne: "00:00",
+        entryTwo: "00:00",
+        exitTwo: "00:00",
+        holiday,
+      };
+      await onAddPointTime(dataTimeFormat);
+      reset();
+      return;
+    }
+
     await onAddPointTime(data);
     reset();
   }
@@ -38,9 +136,25 @@ export function AsideAddPoint() {
 
   const bonusTotalMinutesStatus = Math.sign(bonusTotalMinutes);
 
+  const hours = Math.floor(bonusTotalMinutes / 60)
+    .toString()
+    .padStart(2, "0");
+  const minutes = (bonusTotalMinutes % 60).toString().padStart(2, "0");
+
+  const isValueHasString =
+    watch("entryOne").length === 5 &&
+    watch("entryTwo").length === 5 &&
+    watch("exitOne").length === 5 &&
+    watch("exitTwo").length === 5;
+
   return (
-    <div className="bg-zinc-800 w-[380px] flex flex-col items-center">
-      <span
+    <div className="bg-zinc-800 w-[380px] flex flex-col items-center pt-4">
+      <ClockTimeStatus
+        bonusTotalMinutesStatus={bonusTotalMinutesStatus}
+        hours={hours}
+        minutes={minutes}
+      />
+      {/* <span
         className={`border mt-4 p-1 px-3 rounded bg-zinc-900 text-md flex items-center gap-2 ${
           bonusTotalMinutesStatus === 1
             ? "border-green-700 text-green-400"
@@ -52,7 +166,7 @@ export function AsideAddPoint() {
         {bonusTotalMinutesStatus === -1 && "- "}
         {TimeMinutesToString(Math.abs(bonusTotalMinutes))}
         <Clock size={18} />
-      </span>
+      </span> */}
       <DayPicker
         locale={ptBr}
         mode="single"
@@ -91,7 +205,7 @@ export function AsideAddPoint() {
       />
       <form
         onSubmit={handleSubmit(handleSubmitData)}
-        className="flex flex-col gap-6 w-full px-2"
+        className="flex flex-col gap-6 px-2 w-[290px]"
       >
         <div className="flex justify-center gap-10">
           <div>
@@ -100,6 +214,9 @@ export function AsideAddPoint() {
               disabled={!user?.infoUser}
               id="entryOne"
               register={register("entryOne")}
+              onResetValue={() => {
+                setValue("entryOne", "");
+              }}
             />
           </div>
           <div>
@@ -108,6 +225,9 @@ export function AsideAddPoint() {
               disabled={!user?.infoUser}
               id="exitOne"
               register={register("exitOne")}
+              onResetValue={() => {
+                setValue("exitOne", "");
+              }}
             />
           </div>
         </div>
@@ -119,6 +239,9 @@ export function AsideAddPoint() {
               disabled={!user?.infoUser}
               id="entryTwo"
               register={register("entryTwo")}
+              onResetValue={() => {
+                setValue("entryTwo", "");
+              }}
             />
           </div>
           <div>
@@ -127,15 +250,24 @@ export function AsideAddPoint() {
               disabled={!user?.infoUser}
               id="exitTwo"
               register={register("exitTwo")}
+              onResetValue={() => {
+                setValue("exitTwo", "");
+              }}
             />
           </div>
         </div>
+        <div className="flex items-center gap-2">
+          <input id="holiday" type="checkbox" {...register("holiday")} />
+          <label htmlFor="holiday">Feriado?</label>
+        </div>
 
-        <div className="flex justify-center">
+        <div className="flex justify-center w-full">
           <Button
             type="submit"
-            disabled={!user?.infoUser}
-            classNameStyle="w-full mx-10"
+            disabled={
+              !user?.infoUser || (!isValueHasString && !watch("holiday"))
+            }
+            classNameStyle="w-full"
             statusColor="green"
           >
             Enviar
--- src/components/InputMask.tsx
+++ src/components/InputMask.tsx
@@ -1,32 +1,50 @@
 import { Clock } from "phosphor-react";
-import { InputHTMLAttributes, KeyboardEvent } from "react";
+import { InputHTMLAttributes, KeyboardEvent, useState } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputMaskProps extends InputHTMLAttributes<HTMLInputElement> {
   register: UseFormRegisterReturn;
+  onResetValue: () => void;
 }
 
-export function InputMask({ register, ...rest }: InputMaskProps) {
+export function InputMask({ register, onResetValue, ...rest }: InputMaskProps) {
+  const [value, setValue] = useState("");
+
   function handleKeyUpChange(event: KeyboardEvent<HTMLInputElement>) {
     event.currentTarget.maxLength = 5;
     let value = event.currentTarget.value;
     value = value.replace(/\D/g, "");
     value = value.replace(/^(\d{2})(\d)/, "$1:$2");
     event.currentTarget.value = value;
+    setValue(value);
   }
+
+  function handleResetValue() {
+    setValue("");
+    onResetValue();
+  }
+
+  console.log("value", value);
   return (
     <div className="text-gray-100 relative group-[disabled] w-[110px] flex items-center rounded">
       <input
-        className="bg-transparent w-full outline-none py-2 px-4 placeholder:opacity-50 bg-zinc-900 disabled:opacity-50 rounded"
+        className={`bg-transparent w-full outline-none py-2 px-4 placeholder:opacity-50 bg-zinc-900 disabled:opacity-50 rounded`}
         placeholder="23:59"
         onKeyUp={handleKeyUpChange}
         {...register}
         {...rest}
       />
       <Clock
-        className="text-gray-700 absolute right-1"
+        className={`absolute right-1 ${
+          value.length === 5
+            ? "text-green-500"
+            : value.length > 5
+            ? "text-red-500"
+            : "text-gray-700"
+        }`}
         size={24}
         weight="bold"
+        onClick={value.length >= 5 ? handleResetValue : () => {}}
       />
     </div>
   );
